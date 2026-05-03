865e12111162b3e9b79befd79bef5bce54839cd3
Boricaweb <thanapolsrangkaew@gmail.com>
2026-05-03 22:02:11 +0700

    1st commit start coding navbar

---
+++ src/components/homepage/navBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <nav className="bg-rose-200 h-[10vh] w-screen flex items-start justify-center flex-col gap-4 p-4">
+      <div className="h-[50%] w-[15%] flex items-center justify-center border-red-500 border-2">
+        <h1 className="w-[90%] text-[2rem] font-bold text-red-500">
+          SHOPPER
+        </h1>
+      </div>     
+    </nav>
+  );
+}
